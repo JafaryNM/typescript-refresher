9c91d415a85214b219d31f677db4d94a66abe530
JAFARY <jaffarymdegela@gmail.com>
2025-09-20 18:15:37 +0300

    annotation-varibale

---
+++ annotations/variable.ts
@@ -0,0 +1,19 @@
+const apple: number = 5;
+
+let appleTest: number = 28;
+
+//  You can change  value
+appleTest = 78;
+
+// Not allowed in typescript if
+
+appleTest = "Apple";
+
+let speed: string = "fast";
+let hasName: boolean = true;
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+//  Builting objects
+
+let now: Date = new Date();
