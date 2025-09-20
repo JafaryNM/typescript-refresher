3628aa45934dbfa183ad6cbe98ad694e9dcf5da6
JAFARY <jaffarymdegela@gmail.com>
2025-09-20 18:04:56 +0300

    types-typesript

---
+++ types.ts
@@ -0,0 +1,15 @@
+const today = new Date();
+today.getMonth();
+
+//  Typescript will raised an error when using un available methods in date
+today.ekeejr();
+
+const person = {
+  age: 30,
+};
+
+person.keekjrj;
+
+class Color {}
+
+const red = new Color();
